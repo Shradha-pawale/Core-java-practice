e60366af9c8ebfd89f43d11c6a18584f22e02a6a
Shradha-pawale <shradhapawale9@gmail.com>
2026-07-23 17:47:53 +0530

    third commit

---
+++ core_practise/src/core_practise/Main.java
@@ -0,0 +1,21 @@
+package core_practise;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub 
+		
+		 
+		
+	}
+}
+class car{
+		
+}
+class Bike{
+	
+}
+
+class Bus{
+	
+}
